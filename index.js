f819ce497a95c0fc71c2aaf22d8c13abfc57ee7d
chiragbagde <chiragsbagde@gmail.com>
2025-04-20 23:22:53 +0530

    added task for cron job

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
-import cron from "node-cron";
 import { dumpDbtoCloud } from "./b2-cloud-restore.js";
-import dotenv from "dotenv";  
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const DB_NAME = process.env.DB_NAME;
 const OUTPUT_DIR = process.env.OUTPUT_DIR;
 
-cron.schedule("0 0 * * *", async () => {
-  console.log("⏰ Running scheduled task: dumpDbtoCloud");
+(async () => {
+  console.log("⏰ Starting dumpDbtoCloud task");
   try {
-    await dumpDbtoCloud(DB_NAME, OUTPUT_DIR)
-      .then(() => console.log("✅ Migration and upload completed successfully"))
-      .catch((err) => console.error("❌ Migration and upload failed:", err));
-    console.log("✅ dumpDbtoCloud completed successfully.");
+    await dumpDbtoCloud(DB_NAME, OUTPUT_DIR);
+    console.log("✅ Migration and upload completed successfully.");
   } catch (error) {
     console.error("❌ Error running dumpDbtoCloud:", error);
   }
-});
-
-console.log("Cron job scheduled. Waiting for the next execution...");
+})();
